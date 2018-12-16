436792ccebb43faa76d643f20d76da29f71389c4
Logan <logan.maughan@gmail.com>
2018-12-15 17:42:25 -0700

    reformated code

--- src/main/java/com/dnd/dao/UserDao.java
+++ src/main/java/com/dnd/dao/UserDao.java
@@ -3,16 +3,10 @@ package com.dnd.dao;
 import com.dnd.model.User;
 import com.dnd.model.adapter.UserRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
--- src/main/java/com/dnd/utils/Utils.java
+++ src/main/java/com/dnd/utils/Utils.java
@@ -4,7 +4,6 @@ import org.springframework.util.CollectionUtils;
 
 import javax.sql.DataSource;
 import java.sql.Array;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
