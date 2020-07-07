16f1c74138d89cccaf91cb7b45de0c289a53dee1
Logan Maughan <logan.maughan@gmail.com>
2020-07-07 13:57:54 -0700

    .

--- src/main/resources/sql/createDatabase.sql
+++ src/main/resources/sql/createDatabase.sql
@@ -3,125 +3,112 @@ CREATE DATABASE dndtools;
 USE dndtools;
 DROP TABLE IF EXISTS ability_levels;
 DROP TABLE IF EXISTS saving_throws;
-DROP TABLE IF EXISTS abilities;
 DROP TABLE IF EXISTS skills;
 DROP TABLE IF EXISTS action;
 DROP TABLE IF EXISTS monster;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS spell;
+DROP TABLE IF EXISTS campaign;
 
-CREATE TABLE users
-(
-  user_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
-  username text NOT NULL,
-  admin boolean DEFAULT FALSE,
-  first_name text NOT NULL,
-  last_name text NOT NULL
-);
+CREATE TABLE `monster` (
+  `name` varchar(255) NOT NULL,
+  `size` text,
+  `type` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `subtype` text,
+  `alignment` text,
+  `armor_class` smallint DEFAULT NULL,
+  `hit_points` smallint DEFAULT NULL,
+  `hit_dice` text,
+  `speed` text,
+  `senses` text,
+  `languages` text,
+  `challenge_rating` decimal(6,4) DEFAULT NULL,
+  `damage_vulnerabilities` text,
+  `damage_resistances` text,
+  `damage_immunities` text,
+  `condition_immunities` text,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE monster
-(
-  name VARCHAR(255) NOT NULL PRIMARY KEY,
-  created_by SMALLINT NOT NULL,
-  size text,
-  TYPE text,
-  subtype text,
-  alignment text,
-  armor_class SMALLINT,
-  hit_points SMALLINT,
-  hit_dice text,
-  speed text,
-  senses text,
-  languages text,
-  challenge_rating numeric(6,4),
-  damage_vulnerabilities text,
-  damage_resistances text,
-  damage_immunities text,
-  condition_immunities text,
-  FOREIGN KEY(created_by) REFERENCES users(user_id)
-  ON DELETE CASCADE ON UPDATE CASCADE );
+CREATE TABLE `ability_levels` (
+  `strength` smallint DEFAULT NULL,
+  `dexterity` smallint DEFAULT NULL,
+  `constitution` smallint DEFAULT NULL,
+  `wisdom` smallint DEFAULT NULL,
+  `intelligence` smallint DEFAULT NULL,
+  `charisma` smallint DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`name`),
+  CONSTRAINT `ability_levels_ibfk_1` FOREIGN KEY (`name`) REFERENCES `monster` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE ability_levels
-(
-  strength SMALLINT,
-  dexterity SMALLINT,
-  constitution SMALLINT,
-  wisdom SMALLINT,
-  intelligence SMALLINT,
-  charisma SMALLINT,
-  name VARCHAR(255) NOT NULL PRIMARY KEY,
-  FOREIGN KEY(name) REFERENCES monster(name)
-  ON UPDATE CASCADE ON DELETE CASCADE
-);
-
-CREATE TABLE saving_throws
-(
-  strength SMALLINT,
-  dexterity SMALLINT,
-  constitution SMALLINT,
-  wisdom SMALLINT,
-  intelligence SMALLINT,
-  charisma SMALLINT,
-  name VARCHAR(255) NOT NULL PRIMARY KEY,
-  FOREIGN KEY(name) REFERENCES monster(name)
-  ON UPDATE CASCADE ON DELETE CASCADE
-);
+CREATE TABLE `action` (
+  `action_id` smallint NOT NULL AUTO_INCREMENT,
+  `monster_name` varchar(255) NOT NULL,
+  `action_type` smallint NOT NULL,
+  `name` text,
+  `damage_bonus` smallint DEFAULT NULL,
+  `damage_dice` text,
+  `attack_bonus` smallint DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`action_id`),
+  KEY `monster_name` (`monster_name`),
+  CONSTRAINT `action_ibfk_1` FOREIGN KEY (`monster_name`) REFERENCES `monster` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1477 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE skills
-(
-  name VARCHAR(255) NOT NULL PRIMARY KEY,
-  acrobatics SMALLINT,
-  animal_handling SMALLINT,
-  arcana SMALLINT,
-  athletics SMALLINT,
-  deception SMALLINT,
-  history SMALLINT,
-  insight SMALLINT,
-  intimidation SMALLINT,
-  investigation SMALLINT,
-  medicine SMALLINT,
-  nature SMALLINT,
-  perception SMALLINT,
-  performance SMALLINT,
-  persuasion SMALLINT,
-  religion SMALLINT,
-  slight_of_hand SMALLINT,
-  stealth SMALLINT,
-  survival SMALLINT,
-  FOREIGN KEY(name) REFERENCES monster(name)
-  ON UPDATE CASCADE ON DELETE CASCADE
-);
+CREATE TABLE `saving_throws` (
+  `strength` smallint DEFAULT NULL,
+  `dexterity` smallint DEFAULT NULL,
+  `constitution` smallint DEFAULT NULL,
+  `wisdom` smallint DEFAULT NULL,
+  `intelligence` smallint DEFAULT NULL,
+  `charisma` smallint DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`name`),
+  CONSTRAINT `saving_throws_ibfk_1` FOREIGN KEY (`name`) REFERENCES `monster` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE action
-(
-  action_id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  monster_name VARCHAR(255) NOT NULL,
-  action_type SMALLINT NOT NULL,
-  name text,
-  damage_bonus SMALLINT,
-  damage_dice text,
-  attack_bonus SMALLINT,
-  description text,
-  FOREIGN KEY(monster_name) REFERENCES monster(name)
-  ON UPDATE CASCADE ON DELETE CASCADE
-);
+CREATE TABLE `skills` (
+  `name` varchar(255) NOT NULL,
+  `acrobatics` smallint DEFAULT NULL,
+  `animal_handling` smallint DEFAULT NULL,
+  `arcana` smallint DEFAULT NULL,
+  `athletics` smallint DEFAULT NULL,
+  `deception` smallint DEFAULT NULL,
+  `history` smallint DEFAULT NULL,
+  `insight` smallint DEFAULT NULL,
+  `intimidation` smallint DEFAULT NULL,
+  `investigation` smallint DEFAULT NULL,
+  `medicine` smallint DEFAULT NULL,
+  `nature` smallint DEFAULT NULL,
+  `perception` smallint DEFAULT NULL,
+  `performance` smallint DEFAULT NULL,
+  `persuasion` smallint DEFAULT NULL,
+  `religion` smallint DEFAULT NULL,
+  `slight_of_hand` smallint DEFAULT NULL,
+  `stealth` smallint DEFAULT NULL,
+  `survival` smallint DEFAULT NULL,
+  PRIMARY KEY (`name`),
+  CONSTRAINT `skills_ibfk_1` FOREIGN KEY (`name`) REFERENCES `monster` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE spell
-(
-  spell_id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name text NOT NULL,
-  description text NOT NULL,
-  page text,
-  spell_range text,
-  components text,
-  materials text,
-  ritual BOOLEAN DEFAULT FALSE,
-  duration text,
-  concentration BOOLEAN NOT NULL DEFAULT FALSE,
-  casting_time text NOT NULL,
-  level SMALLINT NOT NULL,
-  school text,
-  classes text,
-  higher_level text
-);
+CREATE TABLE `spell` (
+  `spell_id` smallint NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `page` text,
+  `spell_range` text,
+  `components` text,
+  `materials` text,
+  `ritual` tinyint(1) DEFAULT '0',
+  `duration` text,
+  `concentration` tinyint(1) NOT NULL DEFAULT '0',
+  `casting_time` text NOT NULL,
+  `level` smallint NOT NULL,
+  `school` text,
+  `classes` text,
+  `higher_level` text,
+  PRIMARY KEY (`spell_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1243 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
--- src/test/java/com/dnd/ReadJsonFile.java
+++ src/test/java/com/dnd/ReadJsonFile.java
@@ -2,10 +2,14 @@ package com.dnd;
 
 import com.dnd.dao.SpellDao;
 import com.dnd.model.Spell;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -22,12 +27,14 @@ public class ReadJsonFile {
 
     @Autowired
     private SpellDao dao;
+    @Autowired
+    private ObjectMapper mapper;
 
     @Test
     public void insertSpells() throws Exception {
         URL url = Resources.getResource("spells.json");
         String json = Resources.toString(url, Charsets.UTF_8);
-        List<Spell> spells = new Gson().fromJson(json, new TypeToken<List<Spell>>(){}.getType());
+        List<Spell> spells = new Gson().fromJson(json, new TypeToken<List<Spell>>() {}.getType());
 
         spells.forEach(spell -> {
             try {
@@ -39,4 +46,6 @@ public class ReadJsonFile {
         });
 
     }
+
+
 }
